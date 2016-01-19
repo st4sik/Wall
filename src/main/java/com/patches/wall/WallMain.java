839a6df3bc77a60c8d9518b6b0ee1ad3ecfbf077
st4sik <ryabokonsm@mail.ru>
2016-01-19 23:50:56 +0300

    Created UI.

---
+++ src/main/java/com/patches/wall/WallMain.java
@@ -0,0 +1,11 @@
+package com.patches.wall;
+
+/**
+ * Created by STASСтас on 1/19/2016.
+ */
+public class WallMain {
+    public static void main (String argv[]){
+        new WallFrame();
+
+    }
+}
